8c03209e447f4efb38ed1fa91715e0b20c7b2b15
gcross <gcross@phys.washington.edu>
2011-01-10 22:42:53 -0800

    Turned the auxiliary data structures for the pauli groups into local varargs.

--- sources/common/constraints/ordering/row/pauli_groups.cpp
+++ sources/common/constraints/ordering/row/pauli_groups.cpp
@@ -7,7 +7,6 @@
 #include <gecode/minimodel.hh>
 
 #include "constraints/ordering/row/pauli_groups.hpp"
-#include "column_pauli_sets.hpp"
 #include "utilities.hpp"
 //@-<< Includes >>
 
@@ -19,172 +18,166 @@ using namespace Gecode;
 //@-<< Usings >>
 
 //@+others
-//@+node:gcross.20101229110857.2587: ** class PauliGroupsRowOrderingData
-//@+node:gcross.20101229110857.2588: *3* (constructors)
-PauliGroupsRowOrderingData::PauliGroupsRowOrderingData(
-      OperatorSpace& space
-    , ColumnPauliSetsData& pauli_column_sets_data
-    , unsigned int start_column
-    , unsigned int end_column
-    , unsigned int start_row
-    , unsigned int end_row
-    )
-    : number_of_columns(end_column-start_column)
-    , number_of_rows(end_row-start_row)
-    , pauli_orderings(space,number_of_columns*4,0,3)
-    , unsorted_orderings(space,number_of_columns*number_of_rows,0,3)
-    , orderings(space,number_of_columns*number_of_rows,0,3)
-    , X_set_sizes(space,number_of_columns,0,number_of_rows)
-    , Y_set_sizes(space,number_of_columns,0,number_of_rows)
-    , Z_set_sizes(space,number_of_columns,0,number_of_rows)
-{
-    IntMatrix  pauli_orderings_matrix(pauli_orderings,4,number_of_columns),
-               unsorted_orderings_matrix(unsorted_orderings,number_of_rows,number_of_columns),
-               orderings_matrix = getOrderingsMatrix(),
-               O_matrix = space.getOMatrix().slice(start_column,end_column,start_row,end_row);
-    BOOST_FOREACH(const unsigned int i, irange(0u,number_of_columns)) {
-        cardinality(space,pauli_column_sets_data.X_sets[i],X_set_sizes[i]);
-        cardinality(space,pauli_column_sets_data.Y_sets[i],Y_set_sizes[i]);
-        cardinality(space,pauli_column_sets_data.Z_sets[i],Z_set_sizes[i]);
-        rel(space,
-            (X_set_sizes[i] < Z_set_sizes[i] && (
-                (Z_set_sizes[i] <  Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 1
-                 && pauli_orderings_matrix(2,i) == 2
-                 && pauli_orderings_matrix(3,i) == 3
-                )
-             ^  (Z_set_sizes[i] == Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 1
-                 && pauli_orderings_matrix(2,i) == 2
-                 && pauli_orderings_matrix(3,i) == 2
-                )
-             ^  (Z_set_sizes[i] >  Y_set_sizes[i] && (
-                    (X_set_sizes[i] <  Y_set_sizes[i]
-                     && pauli_orderings_matrix(0,i) == 0
-                     && pauli_orderings_matrix(1,i) == 1
-                     && pauli_orderings_matrix(2,i) == 3
-                     && pauli_orderings_matrix(3,i) == 2
-                    )
-                 ^  (X_set_sizes[i] == Y_set_sizes[i]
-                     && pauli_orderings_matrix(0,i) == 0
-                     && pauli_orderings_matrix(1,i) == 1
-                     && pauli_orderings_matrix(2,i) == 2
-                     && pauli_orderings_matrix(3,i) == 1
-                    )
-                 ^  (X_set_sizes[i] >  Y_set_sizes[i]
-                     && pauli_orderings_matrix(0,i) == 0
-                     && pauli_orderings_matrix(1,i) == 2
-                     && pauli_orderings_matrix(2,i) == 3
-                     && pauli_orderings_matrix(3,i) == 1
-                    )
-                ))
-            ))
-         ^  (X_set_sizes[i] == Z_set_sizes[i] && (
-                (Z_set_sizes[i] <  Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 1
-                 && pauli_orderings_matrix(2,i) == 1
-                 && pauli_orderings_matrix(3,i) == 2
-                )
-             ^  (Z_set_sizes[i] == Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 1
-                 && pauli_orderings_matrix(2,i) == 1
-                 && pauli_orderings_matrix(3,i) == 1
-                )
-             ^  (Z_set_sizes[i] >  Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 2
-                 && pauli_orderings_matrix(2,i) == 2
-                 && pauli_orderings_matrix(3,i) == 1
-                )
-            ))
-         ^  (X_set_sizes[i] > Z_set_sizes[i] && (
-                (Z_set_sizes[i] <  Y_set_sizes[i] && (
-                    (X_set_sizes[i] <  Y_set_sizes[i]
-                     && pauli_orderings_matrix(0,i) == 0
-                     && pauli_orderings_matrix(1,i) == 2
-                     && pauli_orderings_matrix(2,i) == 1
-                     && pauli_orderings_matrix(3,i) == 3
-                    )
-                 ^  (X_set_sizes[i] == Y_set_sizes[i]
-                     && pauli_orderings_matrix(0,i) == 0
-                     && pauli_orderings_matrix(1,i) == 2
-                     && pauli_orderings_matrix(2,i) == 1
-                     && pauli_orderings_matrix(3,i) == 2
-                    )
-                 ^  (X_set_sizes[i] >  Y_set_sizes[i]
-                     && pauli_orderings_matrix(0,i) == 0
-                     && pauli_orderings_matrix(1,i) == 3
-                     && pauli_orderings_matrix(2,i) == 1
-                     && pauli_orderings_matrix(3,i) == 2
-                    )
-                ))
-             ^  (Z_set_sizes[i] == Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 2
-                 && pauli_orderings_matrix(2,i) == 1
-                 && pauli_orderings_matrix(3,i) == 1
-                )
-             ^  (Z_set_sizes[i] >  Y_set_sizes[i]
-                 && pauli_orderings_matrix(0,i) == 0
-                 && pauli_orderings_matrix(1,i) == 3
-                 && pauli_orderings_matrix(2,i) == 2
-                 && pauli_orderings_matrix(3,i) == 1
-                )
-            ))
-        );
-        BOOST_FOREACH(const unsigned int j, irange(0u,number_of_rows)) {
-            element(space,pauli_orderings_matrix.row(i),O_matrix(i,j),unsorted_orderings_matrix(j,i));
-        }
-    }
-    BOOST_FOREACH(const unsigned int i, irange(0u,number_of_rows)) {
-        sorted(space,unsorted_orderings_matrix.col(i),orderings_matrix.col(i));
-    }
-}
-
-PauliGroupsRowOrderingData::PauliGroupsRowOrderingData(bool share, OperatorSpace& space, PauliGroupsRowOrderingData& my)
-    : number_of_columns(my.number_of_columns)
-    , number_of_rows(my.number_of_rows)
-{
-    pauli_orderings.update(space,share,my.pauli_orderings);
-    unsorted_orderings.update(space,share,my.unsorted_orderings);
-    orderings.update(space,share,my.orderings);
-    X_set_sizes.update(space,share,my.X_set_sizes);
-    Y_set_sizes.update(space,share,my.Y_set_sizes);
-    Z_set_sizes.update(space,share,my.Z_set_sizes);
-}
-//@+node:gcross.20101229110857.2619: *3* copy
-auto_ptr<OperatorSpace::AuxiliaryData> PauliGroupsRowOrderingData::copy(const bool share, OperatorSpace& space) {
-    return wrapAutoPtr(dynamic_cast<OperatorSpace::AuxiliaryData*>(new PauliGroupsRowOrderingData(share,space,*this)));
-}
-//@+node:gcross.20101229110857.2615: *3* getOrderingsMatrix
-IntMatrix PauliGroupsRowOrderingData::getOrderingsMatrix() {
-    return IntMatrix(orderings,number_of_rows,number_of_columns);
-}
 //@+node:gcross.20101231214817.2266: ** Functions
 //@+node:gcross.20101229110857.2617: *3* postPauliGroupsRowOrderingConstraint
 BoolVarArgs postPauliGroupsRowOrderingConstraint(
       OperatorSpace& space
-    , const unsigned int starting_column
-    , const unsigned int ending_column
-    , const unsigned int starting_row
-    , const unsigned int ending_row
+    , const unsigned int start_column
+    , const unsigned int end_column
+    , const unsigned int start_row
+    , const unsigned int end_row
     , BoolVarArgs initial_ties
 ) {
-    if(ending_column-starting_column <= 0 || ending_row-starting_row <= 0) return BoolVarArgs();
+    const unsigned int number_of_rows = end_row-start_row
+                     , number_of_columns = end_column-start_column;
+                     ;
+    if(number_of_columns <= 0 || number_of_rows <= 0) return BoolVarArgs();
 
-    auto_ptr<ColumnPauliSetsData> pauli_column_sets_data(new ColumnPauliSetsData(space,starting_column,ending_column,starting_row,ending_row));
-    auto_ptr<PauliGroupsRowOrderingData> pauli_groups_ordering_data(new PauliGroupsRowOrderingData(space,*pauli_column_sets_data,starting_column,ending_column,starting_row,ending_row));
+    IntVarArgs orderings(space,number_of_columns*number_of_rows,0,3);
+    IntMatrix orderings_matrix(orderings,number_of_rows,number_of_columns);
 
-    BoolVarArgs final_ties = postOrderingConstraint(space,pauli_groups_ordering_data->getOrderingsMatrix());
+    {
+        //@+<< Compute X/Y/Z sets >>
+        //@+node:gcross.20110110211728.1584: *4* << Compute X/Y/Z sets >>
+        SetVarArgs X_sets(space,number_of_columns,IntSet::empty,IntSet(0,number_of_rows))
+                 , Y_sets(space,number_of_columns,IntSet::empty,IntSet(0,number_of_rows))
+                 , Z_sets(space,number_of_columns,IntSet::empty,IntSet(0,number_of_rows))
+                 ;
+        {
+            SetVarArgs X_bit_sets(space,number_of_columns,IntSet::empty,IntSet(0,number_of_rows))
+                     , Z_bit_sets(space,number_of_columns,IntSet::empty,IntSet(0,number_of_rows))
+                     ;
+            BoolMatrix X_matrix = space.getXMatrix(),
+                       Z_matrix = space.getZMatrix(),
+                       non_trivial_matrix = space.getNonTrivialMatrix();
+            BOOST_FOREACH(const unsigned int i, irange(0u,number_of_columns)) {
+                channel(space,X_matrix.slice(start_column+i,start_column+i+1,start_row,end_row),X_bit_sets[i]);
+                channel(space,Z_matrix.slice(start_column+i,start_column+i+1,start_row,end_row),Z_bit_sets[i]);
+                X_sets[i] = expr(space,X_bit_sets[i] - Z_bit_sets[i]);
+                Z_sets[i] = expr(space,Z_bit_sets[i] - X_bit_sets[i]);
+                Y_sets[i] = expr(space,X_bit_sets[i] & Z_bit_sets[i]);
+            }
+        }
+        //@-<< Compute X/Y/Z sets >>
+        //@+<< Populate orderings matrix >>
+        //@+node:gcross.20110110211728.1585: *4* << Populate orderings matrix >>
+        {
+            IntVarArgs pauli_orderings(space,number_of_columns*4,0,3)
+                     , unsorted_orderings(space,number_of_columns*number_of_rows,0,3)
+                     , X_set_sizes(space,number_of_columns,0,number_of_rows)
+                     , Y_set_sizes(space,number_of_columns,0,number_of_rows)
+                     , Z_set_sizes(space,number_of_columns,0,number_of_rows)
+                     ;
+            IntMatrix pauli_orderings_matrix(pauli_orderings,4,number_of_columns)
+                    , unsorted_orderings_matrix(unsorted_orderings,number_of_rows,number_of_columns)
+                    , O_matrix = space.getOMatrix().slice(start_column,end_column,start_row,end_row)
+                    ;
+            BOOST_FOREACH(const unsigned int i, irange(0u,number_of_columns)) {
+                cardinality(space,X_sets[i],X_set_sizes[i]);
+                cardinality(space,Y_sets[i],Y_set_sizes[i]);
+                cardinality(space,Z_sets[i],Z_set_sizes[i]);
+                rel(space,
+                    (X_set_sizes[i] < Z_set_sizes[i] && (
+                        (Z_set_sizes[i] <  Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 1
+                         && pauli_orderings_matrix(2,i) == 2
+                         && pauli_orderings_matrix(3,i) == 3
+                        )
+                     ^  (Z_set_sizes[i] == Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 1
+                         && pauli_orderings_matrix(2,i) == 2
+                         && pauli_orderings_matrix(3,i) == 2
+                        )
+                     ^  (Z_set_sizes[i] >  Y_set_sizes[i] && (
+                            (X_set_sizes[i] <  Y_set_sizes[i]
+                             && pauli_orderings_matrix(0,i) == 0
+                             && pauli_orderings_matrix(1,i) == 1
+                             && pauli_orderings_matrix(2,i) == 3
+                             && pauli_orderings_matrix(3,i) == 2
+                            )
+                         ^  (X_set_sizes[i] == Y_set_sizes[i]
+                             && pauli_orderings_matrix(0,i) == 0
+                             && pauli_orderings_matrix(1,i) == 1
+                             && pauli_orderings_matrix(2,i) == 2
+                             && pauli_orderings_matrix(3,i) == 1
+                            )
+                         ^  (X_set_sizes[i] >  Y_set_sizes[i]
+                             && pauli_orderings_matrix(0,i) == 0
+                             && pauli_orderings_matrix(1,i) == 2
+                             && pauli_orderings_matrix(2,i) == 3
+                             && pauli_orderings_matrix(3,i) == 1
+                            )
+                        ))
+                    ))
+                 ^  (X_set_sizes[i] == Z_set_sizes[i] && (
+                        (Z_set_sizes[i] <  Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 1
+                         && pauli_orderings_matrix(2,i) == 1
+                         && pauli_orderings_matrix(3,i) == 2
+                        )
+                     ^  (Z_set_sizes[i] == Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 1
+                         && pauli_orderings_matrix(2,i) == 1
+                         && pauli_orderings_matrix(3,i) == 1
+                        )
+                     ^  (Z_set_sizes[i] >  Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 2
+                         && pauli_orderings_matrix(2,i) == 2
+                         && pauli_orderings_matrix(3,i) == 1
+                        )
+                    ))
+                 ^  (X_set_sizes[i] > Z_set_sizes[i] && (
+                        (Z_set_sizes[i] <  Y_set_sizes[i] && (
+                            (X_set_sizes[i] <  Y_set_sizes[i]
+                             && pauli_orderings_matrix(0,i) == 0
+                             && pauli_orderings_matrix(1,i) == 2
+                             && pauli_orderings_matrix(2,i) == 1
+                             && pauli_orderings_matrix(3,i) == 3
+                            )
+                         ^  (X_set_sizes[i] == Y_set_sizes[i]
+                             && pauli_orderings_matrix(0,i) == 0
+                             && pauli_orderings_matrix(1,i) == 2
+                             && pauli_orderings_matrix(2,i) == 1
+                             && pauli_orderings_matrix(3,i) == 2
+                            )
+                         ^  (X_set_sizes[i] >  Y_set_sizes[i]
+                             && pauli_orderings_matrix(0,i) == 0
+                             && pauli_orderings_matrix(1,i) == 3
+                             && pauli_orderings_matrix(2,i) == 1
+                             && pauli_orderings_matrix(3,i) == 2
+                            )
+                        ))
+                     ^  (Z_set_sizes[i] == Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 2
+                         && pauli_orderings_matrix(2,i) == 1
+                         && pauli_orderings_matrix(3,i) == 1
+                        )
+                     ^  (Z_set_sizes[i] >  Y_set_sizes[i]
+                         && pauli_orderings_matrix(0,i) == 0
+                         && pauli_orderings_matrix(1,i) == 3
+                         && pauli_orderings_matrix(2,i) == 2
+                         && pauli_orderings_matrix(3,i) == 1
+                        )
+                    ))
+                );
+                BOOST_FOREACH(const unsigned int j, irange(0u,number_of_rows)) {
+                    element(space,pauli_orderings_matrix.row(i),O_matrix(i,j),unsorted_orderings_matrix(j,i));
+                }
+            }
+            BOOST_FOREACH(const unsigned int i, irange(0u,number_of_rows)) {
+                sorted(space,unsorted_orderings_matrix.col(i),orderings_matrix.col(i));
+            }
+        }
+        //@-<< Populate orderings matrix >>
+    }
 
-    space.attachAuxiliaryData((auto_ptr<OperatorSpace::AuxiliaryData>)pauli_column_sets_data);
-    space.attachAuxiliaryData((auto_ptr<OperatorSpace::AuxiliaryData>)pauli_groups_ordering_data);
-
-    return final_ties;
+    return postOrderingConstraint(space,orderings_matrix);
 }
 //@+node:gcross.20101231214817.2248: *3* postPauliGroupsRowOrderingConstraints
 StandardFormTies postPauliGroupsRowOrderingConstraints(
