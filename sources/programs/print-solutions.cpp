cdc06c72bc8ecd149dd462427cd48be3644e98c6
gcross <gcross@phys.washington.edu>
2011-01-20 09:32:49 -0800

    Added program print-solutions.

---
+++ sources/programs/print-solutions.cpp
@@ -0,0 +1,69 @@
+//@+leo-ver=5-thin
+//@+node:gcross.20110119225928.1517: * @thin print-solutions.cpp
+//@@language cplusplus
+
+//@+<< Includes >>
+//@+node:gcross.20110119225928.1518: ** << Includes >>
+#include <boost/format.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/range.hpp>
+#include <cstdlib>
+#include <iostream>
+
+#include "boost/local/function.hpp"
+#include "constraints.hpp"
+#include "codequest.hpp"
+#include "utilities.hpp"
+
+using namespace CodeSearch;
+using namespace CodeQuest;
+using namespace Gecode;
+using namespace boost;
+using namespace boost::lambda;
+using namespace boost::range;
+using namespace std;
+//@-<< Includes >>
+
+//@+others
+//@+node:gcross.20110119225928.1519: ** main
+int main(int argc, char** argv) {
+    if(argc != 4) {
+        cout << "Usage: print-solutions <# of qubits> <# of operators> <x bit diagonal size>" << endl;
+        return 1;
+    }
+    const unsigned int  number_of_qubits = atoi(argv[1])
+                      , number_of_operators = atoi(argv[2])
+                      , x_bit_diagonal_size = atoi(argv[3])
+                      ;
+    if(x_bit_diagonal_size > min(number_of_qubits,number_of_operators)) {
+        cout << (format("%1% > min(%2%,%3%)") % x_bit_diagonal_size % number_of_qubits % number_of_operators) << endl;
+        return 2;
+    }
+    if(x_bit_diagonal_size < (number_of_operators+1)/2) {
+        cout << (format("%1% < (%2%+1)/2") % x_bit_diagonal_size % ((number_of_operators+1)/2)) << endl;
+        return 3;
+    }
+    StandardFormParameters parameters(x_bit_diagonal_size,number_of_operators-x_bit_diagonal_size);
+    assert(parameters.x_bit_diagonal_size >= parameters.z_bit_diagonal_size);
+    BOOST_LOCAL_FUNCTION(
+        (void) (processSolution)(
+            (const StandardFormParameters&)(parameters)
+            (const OperatorSpace&)(space)
+        )
+    ) {
+        cout << "-";
+        auto_ptr<dynamic_operator_vector> operators = space.getOperators<dynamic_operator_vector>();
+        BOOST_FOREACH(dynamic_quantum_operator op, *operators) {
+            cout << " - " << op << endl << " ";
+        }
+        cout << endl;
+    } BOOST_LOCAL_FUNCTION_END(processSolution);
+    forEachStandardFormSolution(
+         number_of_qubits
+        ,number_of_operators
+        ,all_constraints
+        ,processSolution
+        );
+}
+//@-others
+//@-leo
