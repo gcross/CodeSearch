678d8b50c845cd17db5139c10e5d64244a156c78
Gregory Crosswhite <gcross@phys.washington.edu>
2011-01-20 15:57:33 -0800

    Modified print-solutions to make the last argument x_bit_diagonal_size optional.

--- sources/programs/print-solutions.cpp
+++ sources/programs/print-solutions.cpp
@@ -6,7 +6,7 @@
 //@+node:gcross.20110119225928.1518: ** << Includes >>
 #include <boost/format.hpp>
 #include <boost/lambda/lambda.hpp>
-#include <boost/range.hpp>
+#include <boost/range/algorithm/for_each.hpp>
 #include <cstdlib>
 #include <iostream>
 
@@ -28,42 +28,54 @@ using namespace std;
 //@+others
 //@+node:gcross.20110119225928.1519: ** main
 int main(int argc, char** argv) {
-    if(argc != 4) {
-        cout << "Usage: print-solutions <# of qubits> <# of operators> <x bit diagonal size>" << endl;
+    if(argc < 3 || argc > 4) {
+        cout << "Usage: print-solutions <# of qubits> <# of operators> [x bit diagonal size]" << endl;
         return 1;
     }
     const unsigned int  number_of_qubits = atoi(argv[1])
                       , number_of_operators = atoi(argv[2])
-                      , x_bit_diagonal_size = atoi(argv[3])
                       ;
-    if(x_bit_diagonal_size > min(number_of_qubits,number_of_operators)) {
-        cout << (format("%1% > min(%2%,%3%)") % x_bit_diagonal_size % number_of_qubits % number_of_operators) << endl;
-        return 2;
-    }
-    if(x_bit_diagonal_size < (number_of_operators+1)/2) {
-        cout << (format("%1% < (%2%+1)/2") % x_bit_diagonal_size % ((number_of_operators+1)/2)) << endl;
-        return 3;
-    }
-    StandardFormParameters parameters(x_bit_diagonal_size,number_of_operators-x_bit_diagonal_size);
-    assert(parameters.x_bit_diagonal_size >= parameters.z_bit_diagonal_size);
-    BOOST_FOREACH(
-         const OperatorSpace& space
-        ,generateSolutionsFor(
-            createConstrainedSpace(
-                 number_of_qubits
-                ,number_of_operators
-                ,all_constraints
-                ,parameters
-                )
-         )
+    BOOST_LOCAL_FUNCTION(
+        (void) (process)(
+            (const StandardFormParameters&)(parameters)
+            (const bind)((number_of_qubits)(number_of_operators))
+        )
     ) {
-        cout << "-";
-        auto_ptr<dynamic_operator_vector> operators = space.getOperators<dynamic_operator_vector>();
-        BOOST_FOREACH(dynamic_quantum_operator op, *operators) {
-            cout << " - " << op << endl << " ";
+        assert(parameters.x_bit_diagonal_size >= parameters.z_bit_diagonal_size);
+        BOOST_FOREACH(
+             const OperatorSpace& space
+            ,generateSolutionsFor(
+                createConstrainedSpace(
+                     number_of_qubits
+                    ,number_of_operators
+                    ,all_constraints
+                    ,parameters
+                    )
+             )
+        ) {
+            cout << "-";
+            auto_ptr<dynamic_operator_vector> operators = space.getOperators<dynamic_operator_vector>();
+            BOOST_FOREACH(dynamic_quantum_operator op, *operators) {
+                cout << " - " << op << endl << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
+    } BOOST_LOCAL_FUNCTION_END(process)
+    if(argc == 4) {
+        const unsigned int x_bit_diagonal_size = atoi(argv[3]);
+        if(x_bit_diagonal_size > min(number_of_qubits,number_of_operators)) {
+            cout << (format("%1% > min(%2%,%3%)") % x_bit_diagonal_size % number_of_qubits % number_of_operators) << endl;
+            return 2;
+        }
+        if(x_bit_diagonal_size < (number_of_operators+1)/2) {
+            cout << (format("%1% < (%2%+1)/2") % x_bit_diagonal_size % ((number_of_operators+1)/2)) << endl;
+            return 3;
+        }
+        process(StandardFormParameters(x_bit_diagonal_size,number_of_operators-x_bit_diagonal_size));
+    } else {
+        for_each(generateStandardFormsFor(number_of_qubits,number_of_operators),process);
     }
+    return 0;
 }
 //@-others
 //@-leo
